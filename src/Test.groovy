2c1df57bb49ec972fb76fcce2172d4993b817ac6
zuaa@163.com <aaaaaaaa>
2013-09-22 16:44:32 +0800

    增加代码

---
+++ src/Test.groovy
@@ -0,0 +1,14 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xuping
+ * Date: 13-9-22
+ * Time: 下午3:18
+ * To change this template use File | Settings | File Templates.
+ */
+import com.google.common.base.Strings
+
+println("hello world")
+
+def a="";
+println(Strings.isNullOrEmpty(a))
