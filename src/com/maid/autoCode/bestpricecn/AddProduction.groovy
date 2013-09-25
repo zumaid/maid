9dc3e63cef755dea35b2ba2d1b4c517f1519351b
zuaa <zuaa@163.com>
2013-09-25 22:38:20 +0800

    a

---
+++ src/com/maid/autoCode/bestpricecn/AddProduction.groovy
@@ -0,0 +1,65 @@
+package com.maid.autoCode.bestpricecn
+
+import org.openqa.selenium.Alert
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.Select
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xuping
+ * Date: 13-9-25
+ * Time: 下午9:16
+ * To change this template use File | Settings | File Templates.
+ */
+class AddProduction extends MyWebDriver{
+    static main(args) {
+        def runall="""丝袜 美腿|,2,53,|丝袜
+时尚气质高档风衣|,2,17,|风衣
+性感秋装|,2,20,|连衣裙
+女士性感内裤蕾丝透明|,1,23,|女士内裤
+女士性感高帮鞋蕾丝|,4,38,|女士高帮鞋
+丁字裤|,1,23,|女士内裤"""
+        runall.eachLine {
+            run(it)
+        }
+        f5(driver)
+        quit(driver);
+    }
+    static run(String it){
+
+
+        def keys=it.tokenize("|")[0];
+        def type=it.tokenize("|")[1]
+
+        WebDriver driver = getDriver();
+        login(driver)
+        driver.get("http://www.bestpricecn.com/index.php/tadmin/product/search")
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("${keys}");
+        driver.findElement(By.cssSelector("input.button-style2")).click();
+        driver.findElement(By.cssSelector("td > input[type=\"checkbox\"]")).click();
+        driver.findElement(By.cssSelector("input.button-style")).click();
+        sleep(1000)
+        new Select(driver.findElement(By.xpath("//select[@id='catalog_id']"))).selectByValue(type);
+
+        driver.findElement(By.xpath("//button[2]")).click();
+        sleep(20000)
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+
+           driver.findElement(By.xpath("(//a[contains(text(),'2')])[2]")).click();
+
+           driver.findElement(By.cssSelector("td > input[type=\"checkbox\"]")).click();
+           driver.findElement(By.cssSelector("input.button-style")).click();
+           sleep(1000)
+           new Select(driver.findElement(By.xpath("//select[@id='catalog_id']"))).selectByValue(type);
+           driver.findElement(By.xpath("//button[2]")).click();
+           sleep(20000)
+           alert = driver.switchTo().alert();
+           alert.accept();
+
+        //
+    }
+}
--- src/com/maid/autoCode/bestpricecn/UpdateNews.groovy
+++ src/com/maid/autoCode/bestpricecn/UpdateNews.groovy
@@ -8,28 +8,14 @@ package com.maid.autoCode.bestpricecn
  * To change this template use File | Settings | File Templates.
  */
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import java.util.concurrent.TimeUnit;
-class UpdateNews {
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+class UpdateNews extends MyWebDriver{
     static main(args) {
         run()
     }
     static  void run(){
-        WebDriver driver = new FirefoxDriver( );
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("http://www.bestpricecn.com/index.php/tadmin/login");
-        driver.findElement(By.id("user_name")).sendKeys("zuaa");
-        driver.findElement(By.name("password")).sendKeys("zuaa@428");
-        driver.findElement(By.cssSelector("input[type=\"image\"]")).click();
-        Thread.sleep(10000);
-        println ("等待登陆完成")
-
-
-        rauOneByOne(  driver, "http://www.bestpricecn.com/index.php/tadmin/rule/caiji/3","明星")
+        WebDriver driver = getDriver();
+        login(driver)
         rauOneByOne(  driver, "http://www.bestpricecn.com/index.php/tadmin/rule/caiji/10","明星内衣")
         rauOneByOne(  driver, "http://www.bestpricecn.com/index.php/tadmin/rule/caiji/8","明星内衣")
         rauOneByOne(  driver, "http://www.bestpricecn.com/index.php/tadmin/rule/caiji/9","时尚资讯")
@@ -37,34 +23,9 @@ class UpdateNews {
         rauOneByOne(  driver, "http://www.bestpricecn.com/index.php/tadmin/rule/caiji/6","潮流趋势")
         rauOneByOne(  driver, "http://www.bestpricecn.com/index.php/tadmin/rule/caiji/5","时尚资讯")
         rauOneByOne(  driver, "http://www.bestpricecn.com/index.php/tadmin/rule/caiji/4","潮流趋势")
-
         println ("抓取完成，刷新静态页面")
-
-
-
-        driver.get("http://www.bestpricecn.com/index.php/tadmin/make_html");
-        driver.findElement(By.xpath("//input[2]")).click();
-        driver.findElement(By.id("sbtn")).click();
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-        int ii=0;
-        while(ii<60*5){
-            try {
-                String text=driver.findElement(By.cssSelector("#process > tbody > tr > td")).getText();
-                if(!text.equals("本次页面生成完毕")) {
-                    println ("本次页面没生成完毕 等待1秒钟")
-                    Thread.sleep(1000);
-                }
-            } catch (Error e) {
-                println ("没找到对象 等待1秒钟")
-                Thread.sleep(1000);
-            }
-            ii=ii+1;
-        }
-        Thread.sleep(3000);
-        println("操作完成,等待3秒退出。")
-        driver.quit();
-
+        f5(driver);
+        quit(driver);
     }
 
 
