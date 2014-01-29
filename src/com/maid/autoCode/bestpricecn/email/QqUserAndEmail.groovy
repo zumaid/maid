293ecf20c550136343e7dd986defa203e02cefce
zuaa@163.com <zuaa@163.com>
2014-01-29 13:18:35 +0800

    格式化

--- src/com/maid/autoCode/bestpricecn/email/QqUserAndEmail.groovy
+++ src/com/maid/autoCode/bestpricecn/email/QqUserAndEmail.groovy
@@ -1,7 +1,9 @@
 package com.maid.autoCode.bestpricecn.email
 
+import org.openqa.selenium.By
+
 import com.maid.autoCode.bestpricecn.MyWebDriver
-import org.openqa.selenium.By;/**
+/**
  * Created with IntelliJ IDEA.
  * User: xuping
  * Date: 13-9-30
@@ -10,37 +12,35 @@ import org.openqa.selenium.By;/**
  */
 class QqUserAndEmail extends MyWebDriver {
 
-    static main(args) {
-        driver.get("http://zc.qq.com/chs/index.html");
-        driver.findElement(By.cssSelector("div.box.box_3 > label.item")).click();
-        driver.findElement(By.id("nick")).clear();
-        driver.findElement(By.id("nick")).sendKeys("zuaa");
-        driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("aaaaaaaa");
-        driver.findElement(By.id("password_again")).clear();
-        driver.findElement(By.id("password_again")).sendKeys("aaaaaaaa");
-        driver.findElement(By.id("sex_2")).click();
-        driver.findElement(By.id("year_value")).click();
-        driver.findElement(By.id("year_32")).click();
-        driver.findElement(By.id("month_value")).click();
-        driver.findElement(By.id("month_6")).click();
-        driver.findElement(By.id("day_value")).click();
-        driver.findElement(By.id("day_5")).click();
-        //检测是否存在验证码
-        if ("".equals(driver.findElement(By.id("code")).getAttribute("value"))) {
-            println "验证码存在，需要填写"
-            String code = "";
-            while (code.length() < 4) {
-                code = driver.findElement(By.id("code")).getAttribute("value");
-                sleep(2000)
-            }
-        } else {
-            //暂时不用处理
-        }
-        driver.findElement(By.id("submit")).click();
-        String aaa = driver.findElement(By.id("newUin")).getText();
-        if (aaa != null) {
-            saveMessage "[qq账户信息]" aaa
-        }
-    }
+	static main(args) {
+		driver.get("http://zc.qq.com/chs/index.html");
+		driver.findElement(By.cssSelector("div.box.box_3 > label.item")).click();
+		driver.findElement(By.id("nick")).clear();
+		driver.findElement(By.id("nick")).sendKeys("zuaa");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("aaaaaaaa");
+		driver.findElement(By.id("password_again")).clear();
+		driver.findElement(By.id("password_again")).sendKeys("aaaaaaaa");
+		driver.findElement(By.id("sex_2")).click();
+		driver.findElement(By.id("year_value")).click();
+		driver.findElement(By.id("year_32")).click();
+		driver.findElement(By.id("month_value")).click();
+		driver.findElement(By.id("month_6")).click();
+		driver.findElement(By.id("day_value")).click();
+		driver.findElement(By.id("day_5")).click();
+		//检测是否存在验证码
+		if ("".equals(driver.findElement(By.id("code")).getAttribute("value"))) {
+			println "验证码存在，需要填写"
+			String code = "";
+			while (code.length() < 4) {
+				code = driver.findElement(By.id("code")).getAttribute("value");
+				sleep(2000);
+			}
+		}
+		driver.findElement(By.id("submit")).click();
+		String aaa = driver.findElement(By.id("newUin")).getText();
+		if (aaa != null) {
+			saveMessage "[qq账户信息]" aaa
+		}
+	}
 }
